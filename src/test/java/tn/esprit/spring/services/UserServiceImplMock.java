cca2a7678e5b77e802f8822a32cd35a3a949cbdd
Hbibi-iheb <91993695+Hbibi-iheb@users.noreply.github.com>
2024-10-30 18:14:28 +0100

    Update UserServiceImplMock.java

--- src/test/java/tn/esprit/spring/services/UserServiceImplMock.java
+++ src/test/java/tn/esprit/spring/services/UserServiceImplMock.java
@@ -1,37 +1,47 @@
 package tn.esprit.spring.services;
 
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
+import tn.esprit.spring.repositories.UserRepository;
+import tn.esprit.spring.models.User;
+import tn.esprit.spring.models.Role;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 
 @SpringBootTest
 @TestMethodOrder(OrderAnnotation.class)
 @ExtendWith(MockitoExtension.class)
 class UserServiceImplMock { 
 
-@Mock
-UserRepository userRepository;
+    @Mock
+    UserRepository userRepository;
 
-@InjectMocks
-UserServiceImpl userService;
+    @InjectMocks
+    UserServiceImpl userService;
 
-User user = new User("f1", "l1", new Date(), Role.ADMINISTRATEUR);
-List<User> listUsers = new ArrayList<User>() {
-{
-add(new User("f2", "l2", new Date(), Role.ADMINISTRATEUR));
-add(new User("f3", "l3", new Date(), Role.ADMINISTRATEUR));
-}
+    User user = new User("f1", "l1", new Date(), Role.ADMINISTRATEUR);
+    List<User> listUsers = new ArrayList<User>() {
+        {
+            add(new User("f2", "l2", new Date(), Role.ADMINISTRATEUR));
+            add(new User("f3", "l3", new Date(), Role.ADMINISTRATEUR));
+        }
+    };
 
-};
-
-@Test
-public void testRetrieveUser() {
-Mockito.when(userRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(user));
-User user1 = userService.retrieveUser("2");
-Assertions.assertNotNull(user1);
-}
-
+    @Test
+    public void testRetrieveUser() {
+        Mockito.when(userRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(user));
+        User user1 = userService.retrieveUser("2");
+        assertNotNull(user1);
+    }
 }
